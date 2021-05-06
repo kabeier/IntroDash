edbed2237dc75efe17b09bda4bf4d2cf5b0fae66
Kevin Beier <kabeier@gmail.com>
2021-05-05 20:22:40 -0400

    Scatter chart

--- app.py
+++ app.py
@@ -3,65 +3,47 @@ import dash
 #import dcc to create our graph in our layout
 import dash_core_components as dcc
 #used to generate HTML components
-
-
-
-
-
-
-
-
-
-
 import dash_html_components as html
+#use pandas to read data
+import pandas as pd
+#this will do the plot
+import ploty.graph_objs as go
 
 #init a Dash as app
 app=dash.Dash()
-#Colors dictionary 
-colors={
-    'background' : '#111111',
-    'text' : '#7FDBFF'
-}
-#style here is camelCase but same as css and you use className instead of class
-
-#Build the app layout in HTML with the html import
-    #The HTML tag names are capitalized
-app.layout = html.Div(
-    #Build the page
-    style={'backgroundColor':colors['background']},
-    children=[
-        html.H1(
-            children='Hello Dash',
-            style={
-                'textAlign':'center',
-                'color':colors['text']
-            }
-        ),
-        html.Div(
-            children='Dash: A web application framework for Python.',
-            style={
-                'textAlign' : 'center',
-                'color' : colors['text']
-            }
-        ),
-        #Add the Graph to the page
-        dcc.Graph(
-            id='Graph1',
-            figure={
-                'data':[
-                    {'x':[1,2,3],'y':[4,1,2], 'type':'bar', 'name':'SF'},
-                    {'x':[1,2,3],'y':[2,4,5], 'type':'bar', 'name':'Montreal'}
-                ],
-                'layout':{
-                    'plot_bgcolor' : colors['background'],
-                    'paper_bgcolor' : colors['background'],
-                    'font' : {
-                        'color' : colors['text']
-                    }
-
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data':[go.Scatter(
+                x = df[df['continent'] == i]['gdp per capita'],
+                y = df[df['continent'] == i]['life expectancy'],
+                text = df[df['continent'] == i]['country'],
+                mode = 'markers',
+                opacity = 0.8,
+                marker={
+                    'size':15,
+                    'line':{'width':0.5,'color':'white'}
                 }
-            }
-        )
+                name=i 
+                )
+                for i in df.continent.unique()
+                ],
+            'layout' : go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+                )
+                
+        }
+    )
 ])
 
 if __name__ == '__main__':
